5eef1bebf7292809cd6f37fa6060ab7390c272ed
puneethlakshman05 <puneethlaksh05gmail.com>
2025-07-21 01:17:05 +0530

    Final updations

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -82,7 +82,7 @@ console.log("value of logged in or registered",isAuthenticated)
 
 
     return(
-        <div className="card card-verticalflex flex-col relative  shadow w-[290px] text-sm h-[380px] sm:w-[275px] sm:h-[350px] md:w-[245px] md:h-[350px] lg:w-[260px] lg:h-[410px] hover:transition-transform hover:scale-105 cursor-pointer bg-white p-1 rounded-md">
+        <div className="card card-verticalflex flex-col relative  shadow w-[290px] text-sm h-[380px] sm:w-[275px] sm:h-[350px] md:w-[245px] md:h-[350px] lg:w-[270px] lg:h-[410px] hover:transition-transform hover:scale-105 cursor-pointer bg-white p-1 rounded-md">
      <div className="card-image-container h-[180px]  sm:h-[160px] md:h-[160px] lg:h-[200px]">
           <img className="card-image" src={product.images[0]} alt="shoes" />
      </div>
